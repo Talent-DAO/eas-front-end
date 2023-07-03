a75137b23889598c8b11323c074d74a51fcbbe5d
Jaxcoder <codenamejason@outlook.com>
2023-07-03 16:42:22 -0400

    chore: add useForm/yup hooks/validation

--- packages/nextjs/components/attestation/ReviewForm.tsx
+++ packages/nextjs/components/attestation/ReviewForm.tsx
@@ -1,5 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { yupResolver } from "@hookform/resolvers/yup";
 import { NextPage } from "next";
+import { useForm } from "react-hook-form";
 import { useSigner } from "wagmi";
+import * as yup from "yup";
 import { getEASContract, registerEASSchema } from "~~/services/eas/EAS";
 
 type Comment = {
@@ -27,9 +31,22 @@ type ReviewProps = {
 // Todo:
 // 1. Add form props to match ReviewProps
 // 2. Add form validation
+const schema = yup.object({
+  reviewerId: yup.string().required(),
+  score: yup.number().required(),
+  comment: yup.string().required(),
+});
 
 const ReviewForm: NextPage = () => {
   const { data: signer } = useSigner();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
   const submitReview = async () => {
     console.log("Submitting Review");
@@ -40,45 +57,50 @@ const ReviewForm: NextPage = () => {
     console.log("EAS", { easContract, tx });
   };
 
+  const onSubmit = (data: any) => {
+    console.log("data", data);
+  };
+
   return (
     <div className="grid grid-cols-3">
       <div className="col-span-1" />
       <div className="flex flex-auto items-center justify-center h-screen" data-theme="attestation-form">
-        <div className="border border-stone-900 p-4 px-12 rounded-lg bg-slate-400 text-slate-900">
+        <div className="border border-stone-900 w-[400px] p-4 px-12 rounded-lg bg-slate-400 text-slate-900">
           <div className="text-center">
             <h1 className="text-2xl font-bold">Submit Review</h1>
             <p className="text-md">Submit a publication review</p>
           </div>
           <div className="mt-4">
-            <form
-              onSubmit={e => {
-                e.preventDefault();
-                submitReview();
-              }}
-            >
+            <form onSubmit={handleSubmit(onSubmit)}>
               <div className="flex flex-col">
                 <label className="text-md">Name</label>
                 <input
+                  {...register("reviewerId")}
                   type="text"
                   className="input border rounded-md text-slate-400 mt-2"
-                  placeholder="Enter your name"
+                  placeholder="Enter your address or ENS name"
                   autoComplete="off"
                 />
+                <ErrorFeedback error={errors.reviewerId?.message} />
                 <label className="text-md mt-2">Score</label>
                 <input
+                  {...register("score")}
                   type="number"
-                  max={5}
+                  max={100}
                   min={0}
                   className="input border rounded-md text-slate-400 mt-2"
                   placeholder="Enter your score"
                   autoComplete="off"
                 />
+                <ErrorFeedback error={errors.score?.message} />
                 <label className="text-md mt-2">Review</label>
                 <textarea
+                  {...register("comment")}
                   className="textarea h-24 border rounded-md text-slate-400 mt-2"
                   placeholder="Enter your review"
                   autoComplete="off"
                 />
+                <ErrorFeedback error={errors.comment?.message} />
                 <button type="submit" className="btn btn-primary rounded-md mt-4">
                   Submit
                 </button>
@@ -90,6 +112,10 @@ const ReviewForm: NextPage = () => {
       <div className="col-span-1" />
     </div>
   );
+};
+
+const ErrorFeedback = ({ error }: { error: string | undefined }) => {
+  return <div className="text-red-500">{error}</div>;
 };
 
 export default ReviewForm;
